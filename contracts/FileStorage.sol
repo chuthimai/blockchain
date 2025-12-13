6f242b9f82dbeba8d179ee0bb0357cd4884ada5e
lam-ntt <nguyenthithanhlam2017@gmail.com>
2025-12-13 16:36:26 +0700

    add signature into transaction

--- contracts/FileStorage.sol
+++ contracts/FileStorage.sol
@@ -4,25 +4,30 @@ pragma solidity ^0.8.17;
 contract FileStorage {
     uint256 public fileId;
     string public fileHash;
+    string public fileSignature;
+
 
     event FileStored(
         uint256 fileId,
         string fileHash,
+        string fileSignature,
         uint256 blockId
     );
 
-    function storeFile(uint256 _fileId, string memory _fileHash) public {
+    function storeFile(uint256 _fileId, string memory _fileHash, string memory _fileSignature) public {
         fileId = _fileId;
         fileHash = _fileHash;
+        fileSignature = _fileSignature;
 
         emit FileStored(
             _fileId,
             _fileHash,
+            _fileSignature,
             block.number
         );
     }
 
-    function getFile() public view returns (uint256, string memory) {
-        return (fileId, fileHash);
+    function getFile() public view returns (uint256, string memory, string memory) {
+        return (fileId, fileHash, fileSignature);
     }
 }
